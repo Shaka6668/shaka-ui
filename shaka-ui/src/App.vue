8202ced3a081a8634414af22386e1fd3c9165136
Jin\Jin <656169299@qq.com>
2022-10-10 16:30:35 +0800

    引入jsx，并测试

--- shaka-ui/src/App.vue
+++ shaka-ui/src/App.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/Test'
 </script>
 
 <template>
@@ -12,9 +13,12 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    <Test></Test>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
+
+
 
 <style scoped>
 .logo {
--- shaka-ui/vite.config.ts
+++ shaka-ui/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// 引入jsx，然后在插件里面注册
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  // vueJsx里可以传，也可以不传，推荐不传
+  plugins: [vue(),vueJsx({})]
 })
